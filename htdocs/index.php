8b9aee36e05373f47c4893fc538f3043f445a3a2
Brenden <brenden@brenden.me>
2014-10-23 23:39:01 -0500

    adding fancy launch page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,34 @@
-hello.
+<!html>
+<html> 
+    <head>
+        <style>
+            body {
+                font-family: "Advent Pro", sans-serif;
+                margin: 0;
+                padding: 0;
+                background: #fff;
+            }
+            
+            h1.site-header {
+                display: block;
+                width: 100%
+                height: 50px;
+                background: #6388B1;
+                text-align: center;
+                text-shadow: 0px 3px 4px rgba(0, 0, 0, 0.56);
+            }
+            
+            .launch-message {
+                text-align: center;
+                color: #000;
+            }
+            
+        </style>
+    </head>
+    <body>
+        
+        <h1 class="site-header">Hello!</h1>
+        <p class="launch-message">We know you're excited, but the app hasn't launched the in the App Store yet, but when it does, this page will update.</p>
+        
+    </body>
+</html> 
